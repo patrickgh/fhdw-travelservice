7015193ea048378cfcd762ab64e5a43b5b32c865
Patrick Groß-Holtwick <p.grossholtwick@gmail.com>
2013-08-28 19:27:57 +0200

    added methods to client class

--- fhdw-travelservice-webapp/src/main/java/de/urlaubr/webapp/Client.java
+++ fhdw-travelservice-webapp/src/main/java/de/urlaubr/webapp/Client.java
@@ -2,18 +2,30 @@ package de.urlaubr.webapp;
 
 import de.urlaubr.ws.TravelService;
 import de.urlaubr.ws.client.TravelClient;
+import de.urlaubr.ws.domain.Booking;
+import de.urlaubr.ws.domain.Customer;
+import de.urlaubr.ws.domain.SearchParams;
+import de.urlaubr.ws.domain.Traveler;
 import de.urlaubr.ws.domain.Vacation;
+import org.apache.axis2.AxisFault;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 /**
+ * Wrapper class for client methods.
+ * Useful for converting arrays to required types.
+ * It also enables to change the service class from webservice to direct database access, so that no axis2 is required (for development)
+ *
  * @author Patrick Groß-Holtwick
  *         Date: 27.08.13
  */
 public class Client {
 
-    private static TravelService service = new TravelClient();
+    private static TravelService service = new TravelClient(); //use webservice
+    // private static TravelService service = new TravelServiceImpl(); //use direct database calls
 
     private Client() {}
 
@@ -28,4 +40,39 @@ public class Client {
     public static void logout(Integer sessionKey) {
         service.logout(sessionKey);
     }
+
+    public static List<Booking> getMyVacation(Integer sessionKey) {
+        try {
+            return Arrays.asList(service.getMyVacations(sessionKey));
+        }
+        catch (AxisFault e) {
+            e.printStackTrace();
+        }
+        return Collections.emptyList();
+    }
+
+    public static List<Vacation> findVacations(SearchParams params) {
+        return Arrays.asList(service.findVacations(params));
+    }
+
+    public static Integer createBooking(Integer sessionKey, Integer vacationId, Date startdate, Traveler[] travelers) {
+        return service.createBooking(sessionKey, vacationId, startdate, travelers);
+    }
+
+    public static void cancelBooking(Integer sessionKey, Integer bookingId) {
+        service.cancelBooking(sessionKey, bookingId);
+    }
+
+    public static void rateVacation(Integer sessionKey, Integer bookingId, Integer rating, String comment) {
+        service.rateVacation(sessionKey, bookingId, rating, comment);
+    }
+
+    public static Vacation getVactationById(Integer id) {
+        return service.getVacationById(id);
+    }
+
+    public static Customer getUserInfo(Integer sessionKey) {
+        return service.getUserInfo(sessionKey);
+    }
+
 }
